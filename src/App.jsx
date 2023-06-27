49ece2258316a8fade16521f1deb8e5b057ecc78
pratik.talware <brickfolio_admin@Apples-MacBook-Air.local>
2023-06-27 16:06:42 +0530

    Working env created and navabar added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import AllHomeComponents from './Components/AllHomeComponents'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <AllHomeComponents/>
+    </>
+  )
+}
+
+export default App
